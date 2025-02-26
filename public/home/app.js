cd3b7e2e7296d289331c564f56f49f2b47e7d82d
MohamadMobin Rezaee <134555495+ALONE0007@users.noreply.github.com>
2025-02-27 02:55:39 +0330

    completing hero

--- public/home/app.js
+++ public/home/app.js
@@ -5,17 +5,17 @@ let mobileNavMenuContainer = document.getElementById("mobileNavMenuContainer");
 let mobileMenu = document.querySelectorAll("#mobileMenu");
 
 closingNavMenuPage.addEventListener("click", function () {
-  mobileNavMenuContainer.style.left = "-500px";
-  close.style.display = "none";
-  hamburger.style.display = "block";
-  closingNavMenuPage.classList.replace("visible", "invisible");
-  closingNavMenuPage.classList.replace("opacity-100", "opacity-0");
+    mobileNavMenuContainer.style.left = "-500px";
+    close.style.display = "none";
+    hamburger.style.display = "block";
+    closingNavMenuPage.classList.replace("visible", "invisible");
+    closingNavMenuPage.classList.replace("opacity-100", "opacity-0");
 });
 hamburger.addEventListener("click", function () {
-  mobileNavMenuContainer.style.left = "0px";
-  close.style.display = "block";
-  hamburger.style.display = "none";
-  closingNavMenuPage.classList.replace("invisible", "visible");
+    mobileNavMenuContainer.style.left = "0px";
+    close.style.display = "block";
+    hamburger.style.display = "none";
+    closingNavMenuPage.classList.replace("invisible", "visible");
   closingNavMenuPage.classList.replace("opacity-0", "opacity-100");
 });
 
@@ -28,7 +28,9 @@ close.addEventListener("click", function () {
 // nav mobile menu
 
 mobileMenu.forEach(function (element) {
-  element.addEventListener("click", function () {
+
+  element.addEventListener("click", function (event) {    
+    
     mobileMenu.forEach(function (item) {
       if (item === element) {
         element.nextElementSibling.classList.toggle("activeMenu");
@@ -38,3 +40,4 @@ mobileMenu.forEach(function (element) {
     });
   });
 });
+
